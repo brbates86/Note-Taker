ca086370150a21c30da0d4a3db5c8017f3375e45
Brett Bates <brettbates86@gmail.com>
2022-12-12 12:33:00 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const database = require('./db/db');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
